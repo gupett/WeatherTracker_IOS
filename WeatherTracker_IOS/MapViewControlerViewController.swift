3fbdcba801f98ac997491652c761daf4b06f26f8
Gustav <Gustav@n165-p180.kthopen.kth.se>
2016-04-11 19:06:52 +0200

    Map and location zoom added

---
+++ WeatherTracker_IOS/MapViewControlerViewController.swift
@@ -0,0 +1,82 @@
+//
+//  MapViewControlerViewController.swift
+//  WeatherTracker_IOS
+//
+//  Created by Gustav on 11/04/16.
+//  Copyright © 2016 Gustav. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class MapViewControlerViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+
+    @IBOutlet weak var map: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        //centerMapOnLocation(initialLocation)
+        
+        self.locationManager.delegate = self
+        
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        self.locationManager.startUpdatingLocation()
+        
+        self.map.showsUserLocation = true
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.last
+        
+        zoom(location!.coordinate.latitude, long: location!.coordinate.longitude)
+        
+        self.locationManager.stopUpdatingLocation()
+    }
+    
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        print("error with CoreLocation " + error.localizedDescription)
+        zoom(56.170303, long: 14.863073)
+    }
+    
+    func zoom(lat: CLLocationDegrees, long: CLLocationDegrees) -> Void{
+        //Coordinates
+        //let lat:CLLocationDegrees = 56.170303
+        //let long:CLLocationDegrees = 14.863073
+        let coordinates = CLLocationCoordinate2D(latitude: lat, longitude: long)
+        
+        //Span
+        let latSpan:CLLocationDegrees = 1
+        let longSpan:CLLocationDegrees = 1
+        let span = MKCoordinateSpan(latitudeDelta: latSpan, longitudeDelta: longSpan)
+        
+        //Set area and center of area
+        let region = MKCoordinateRegion(center: coordinates, span: span)
+        
+        //Kartans startvy är nu
+        map.setRegion(region, animated: true)
+        
+        let annotation = MKPointAnnotation()
+        annotation.title = "Karlshamn"
+        annotation.subtitle = "SOL!!!!"
+        annotation.coordinate = coordinates
+        
+        //Create a circle
+        
+        let circle = MKCircle(centerCoordinate: coordinates, radius: 2000)
+        map.addOverlay(circle)
+        
+        map.addAnnotation(annotation)
+        
+    }
+    
+
+
+}
--- WeatherTracker_IOS/ViewController.swift
+++ WeatherTracker_IOS/ViewController.swift
@@ -9,17 +9,62 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var weatherLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func getWeather(sender: AnyObject) {
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    /*func getWeather() -> Void {
+        let stringURL = "http://opendata-download-metfcst.smhi.se/api/category/pmp1.5g/version/1/geopoint/lat/58.59/lon/16.18/data.json"
+        
+        guard let url = NSURL(string: stringURL) else{
+            print("Error creating URL")
+            return
+        }
+        
+        let urlRequest = NSMutableURLRequest(URL: url)
+        urlRequest.HTTPMethod = "GET"
+        
+        let config = NSURLSessionConfiguration.defaultSessionConfiguration()
+        let session = NSURLSession(configuration: config)
+        
+        //let task = session.dataTaskWithRequest(urlRequest, completionHandler: <#T##(NSData?, NSURLResponse?, NSError?) -> Void#>)
+        
+        let task = session.dataTaskWithRequest(urlRequest, completionHandler: { (data, response, erreor) in
+            
+            guard let responseData = data else {
+                print ("No data")
+                return
+            }
+            
+            let jasonResponse: NSDictionary
+            
+            do{
+                jasonResponse = try NSJSONSerialization.JSONObjectWithData(responseData, options: []) as! NSDictionary
+            }catch{
+                print ("error converting to json")
+                return
+            }
+            
+            
+            
+            
+            print (responseData)
+            
+        })
+
+    }*/
 
 }
 
